b0a5e4964d7afa1430e43fe708a1c3d404a512a6
Andre Ziviani <andrepziviani@gmail.com>
2019-07-17 22:33:25 -0300

    fix package name

--- awsfuzzer.go
+++ awsfuzzer.go
@@ -1,4 +1,4 @@
-package awsfuzzer
+package Awsfuzzer
 
 import (
 	"bytes"
--- cmd/aws-fuzzer/main.go
+++ cmd/aws-fuzzer/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/andreziviani/aws-fuzzer"
+	"github.com/AndreZiviani/aws-fuzzer"
 )
 
 func main() {
